85e6f41c91b32aa542546f7541d6f9c59fad4ad1
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-09-29 16:09:56 +0200

    Added a simple todo-list

---
+++ Workshop 2 - Design/source_code/OOP/TodoList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_2
+{
+    class TodoList
+    {
+        //TODO: This is the very stupid way of using the TODO keyword in Visual Studio but it works! For some reason the "User Tasks" don't work on my installation of Visual Studio 2015 Enterprise.
+    }
+}
