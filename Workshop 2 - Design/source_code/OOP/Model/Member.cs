d737424c5842be3d6e88dcb96b53f6d50e5496fc
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-09-30 15:55:06 +0200

    New constructor for using MemberID inside the class

--- Workshop 2 - Design/source_code/OOP/Model/Member.cs
+++ Workshop 2 - Design/source_code/OOP/Model/Member.cs
@@ -9,13 +9,21 @@ namespace Workshop_2.Model
 {
     class Member
     {
+        public int MemberID { get; set; }
         public string Name { get; set; }
         public string SocialSecurityNumber { get; set; }
 
         public Member(string Name, string SocialSecurityNumber)
+            : this(Name, SocialSecurityNumber, 0)
+        {
+
+        }
+
+        public Member(string Name, string SocialSecurityNumber, int MemberID)
         {
             this.Name = Name;
             this.SocialSecurityNumber = SocialSecurityNumber;
+            this.MemberID = MemberID;
         }
     }
 }
