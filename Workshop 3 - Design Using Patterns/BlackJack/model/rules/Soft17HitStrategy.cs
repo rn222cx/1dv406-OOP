c0de79079cc1337da1f8bddd9d5b1739e3ee18a6
Mikael Eriksson <me222wm@student.lnu.se>
2015-10-13 14:43:35 +0200

    Soft17Strategy

---
+++ Workshop 3 - Design Using Patterns/BlackJack/model/rules/Soft17HitStrategy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.model.rules
+{
+    class Soft17HitStrategy : IHitStrategy
+    {
+        // Soft 17 means that the dealer has 17 but in a combination of Ace and 6 (for example Ess, tvåa, tvåa, tvåa). 
+        // This means that the Dealer can get another card valued at 10 but still have 17 as the value of the ace is reduced to 1.
+
+        private const int g_hitLimit = 17;
+
+        public bool DoHit(model.Player a_dealer)
+        {
+            var hand = a_dealer.GetHand();
+            int score = a_dealer.CalcScore();
+
+            // See if one of the dealers card is an Ace.
+            foreach (var card in hand)
+            {
+                // If the dealer has 17.
+                if (score == g_hitLimit)
+                {
+                    // But in a comibination of Ace and 6.
+                    if (card.GetValue() == Card.Value.Ace && score - (int)Card.Value.Ace == 6)
+                    {
+                        // The dealer can get another card valued 10 but still have 17.
+                        score -= 10;
+                    }
+                }
+            }
+
+            return score < g_hitLimit;
+        }
+    }
+}
