cc5b25e56730dbeecaeb47e2a005dffc0d8c77f5
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-10-14 12:10:52 +0200

    Added subject interface

---
+++ Workshop 3 - Design Using Patterns/BlackJack/model/ISubject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.model
+{
+    interface ISubject
+    {
+            void Subscribe(IBlackJackObserver observer);
+            //void Unsubscribe(IBlackJackObserver observer);
+            void Notify();
+    }
+}
