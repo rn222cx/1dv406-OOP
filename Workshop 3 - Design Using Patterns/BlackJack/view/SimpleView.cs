211ab33d845b21a9deb4701a3426a982d2214508
Roy Nilsson <rn222cx@student.lnu.se>
2015-10-15 11:40:41 +0200

    Mega G

--- Workshop 3 - Design Using Patterns/BlackJack/view/SimpleView.cs
+++ Workshop 3 - Design Using Patterns/BlackJack/view/SimpleView.cs
@@ -82,12 +82,42 @@ namespace BlackJack.view
 
         public void PauseGame()
         {
-            Console.Write("Dealing.");
-            Thread.Sleep(400);
-            Console.Write(".");
-            Thread.Sleep(400);
-            Console.Write(".");
-            Thread.Sleep(400);
+            ConsoleSpiner spin = new ConsoleSpiner();
+            Console.Write("Dealing....");
+            int x = 0;
+            while (x < 10)
+            {
+                x++;
+                Thread.Sleep(100);
+                spin.Turn();
+            }
+            //Console.Write("Dealing.");
+            //Thread.Sleep(400);
+            //Console.Write(".");
+            //Thread.Sleep(400);
+            //Console.Write(".");
+            //Thread.Sleep(400);
+        }
+
+        public class ConsoleSpiner
+        {
+            int counter;
+            public ConsoleSpiner()
+            {
+                counter = 0;
+            }
+            public void Turn()
+            {
+                counter++;
+                switch (counter % 4)
+                {
+                    case 0: Console.Write("/"); break;
+                    case 1: Console.Write("-"); break;
+                    case 2: Console.Write("\\"); break;
+                    case 3: Console.Write("|"); break;
+                }
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            }
         }
     }
 }
